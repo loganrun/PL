cc76fc0205bfa278a1ff97b2c49ba38a698a0105
Robert Logan <rob@rob-Dimension-8400.(none)>
2013-09-28 10:50:42 -0700

    Static Pages

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,19 @@
+class StaticPagesController < ApplicationController
+  def index
+  end
+
+  def about
+  end
+
+  def contact
+  end
+
+  def programs
+  end
+
+  def gallery
+  end
+
+  def gallery-details
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pinnacle::Application.config.secret_key_base = 'a4b95a8e7793135c6d27a564586d824f94b286599712101a552b12ded0f1758d3243df616c8bfd44ed9ee23916977c153279ee520a50408f5a487e167808e905'
